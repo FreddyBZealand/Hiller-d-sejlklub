649dc82c633454cf3d14b66672c34fc487e85d84
Marcus Michel Haddad <mah045@edu.zealand.dk>
2025-04-29 11:57:08 +0200

    ..

--- Hillerød sejlklub/Program.cs
+++ Hillerød sejlklub/Program.cs
@@ -9,10 +9,10 @@ namespace Hillerød_sejlklub
         {
             List<Medlem> medlemmer = new List<Medlem>
             {
-                new Medlem(1, "12345678", "anna@example.com"),
-                new Medlem(2, "87654321", "brian@example.com"),
-                new Medlem(3, "11223344", "carla@example.com"),
-                new Medlem(4, "44332211", "dan@example.com")
+                new Medlem(1, "12345678", "anna@example.com", "guldMedlem"),
+                new Medlem(2, "87654321", "brian@example.com", "guldMedlem"),
+                new Medlem(3, "11223344", "carla@example.com", "Medlem"),
+                new Medlem(4, "44332211", "dan@example.com", "guldMedlem")
             };
 
             foreach (var medlem in medlemmer) //Går igennem hvert medlem i listen medlemmer én ad gangen.
