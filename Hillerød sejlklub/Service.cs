9dd441076799002b505218143579bfb5ca654f52
FreddyBZealand <frb009@edu.zealand.dk>
2025-04-29 11:16:49 +0200

    .

--- Hillerød sejlklub/Service.cs
+++ Hillerød sejlklub/Service.cs
@@ -6,7 +6,56 @@ using System.Threading.Tasks;
 
 namespace Hillerød_sejlklub
 {
-    internal class Service
+    public class Service
     {
+        #region Instance fields
+        public string _damage;
+        public string _id;
+        public string _name;
+        public string _type;
+
+
+        #endregion
+
+        #region Constructor
+        public Service(string damage, string id, string name, string type)
+        {
+
+            _damage = damage;
+            _id = id;
+            _name = name;
+            _type = type;
+
+        }
+
+        #endregion
+
+        #region Properties
+        private string damage
+        {
+            get { return _damage; }
+            set { _damage = value; }
+        }
+            private string id
+        { 
+            get { return _id; } 
+            set {  _id = value; } 
+        }
+       
+       private string name
+        { get { return _name; } }
+
+        private string type
+        { get { return _type; } }
+
+
+        #endregion
+
+        #region Methods
+        public override string ToString()
+        {
+            return $"damage : {damage}, id : {id}, name : {name}, type : {type}";
+        }
+        #endregion
     }
 }
