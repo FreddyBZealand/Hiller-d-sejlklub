cc5ff2cde29c6a1a960152e97b1fd009aeb9d38e
Marcus Michel Haddad <mah045@edu.zealand.dk>
2025-04-29 11:52:25 +0200

    ..

--- Hillerød sejlklub/Blog.cs
+++ Hillerød sejlklub/Blog.cs
@@ -8,5 +8,32 @@ namespace Hillerød_sejlklub
 {
     internal class Blog
     {
+        private string Titel { get; set; }
+        private string Indhold { get; set; }
+        private string BilledeSti { get; set; } // f.eks. "billeder/kapsejlads.jpg"
+        private DateTime Dato { get; set; }
+
+        private Blog(string titel, string indhold, string billedeSti)
+        {
+            Titel = titel;
+            Indhold = indhold;
+            BilledeSti = billedeSti;
+            Dato = DateTime.Now;
+        }
+
+        private void Rediger(string nytIndhold, string nyBilledeSti)
+        {
+            Indhold = nytIndhold;
+            BilledeSti = nyBilledeSti;
+            Dato = DateTime.Now;
+        }
+
+        private void VisIndlæg()
+        {
+            Console.WriteLine($"Titel: {Titel}");
+            Console.WriteLine($"Dato: {Dato}");
+            Console.WriteLine($"Indhold: {Indhold}");
+            Console.WriteLine($"Billede: {BilledeSti}");
+        }
     }
 }
