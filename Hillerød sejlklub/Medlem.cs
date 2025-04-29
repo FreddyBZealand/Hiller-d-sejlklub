595a115414428734562110d39a7c2c3fdfcb6234
Marcus Michel Haddad <mah045@edu.zealand.dk>
2025-04-29 10:57:34 +0200

    ..

---
+++ Hillerød sejlklub/Medlem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hillerød_sejlklub
+{
+    internal class Medlem
+    {
+        public int MedlemId { get; set; }
+        public string Telefonnummer { get; set; }
+        public string Email { get; set; }
+
+        public Medlem(int medlemId, string telefonnummer, string email)
+        {
+            MedlemId = medlemId;
+            Telefonnummer = telefonnummer;
+            Email = email;
+        }
+
+        public void VisInfo()
+        {
+            Console.WriteLine($"Medlem ID: {MedlemId}");
+            Console.WriteLine($"Telefonnummer: {Telefonnummer}");
+            Console.WriteLine($"Email: {Email}");
+        }
+    }
+}
+    
+
